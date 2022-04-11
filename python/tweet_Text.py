50e56094356abd0217b3caed0b8d967ce93dbbd4
DESKTOP-ASBNAOK\jihoon <hoonzinope@gmail.com>
2022-04-11 20:11:21 +0900

    코드 수정 (오류 라인 메세지 보내기)

--- python/tweet_Text.py
+++ python/tweet_Text.py
@@ -6,6 +6,7 @@ import json
 import time
 
 _url = ""
+_url2 = ""
 api_key = ""
 secret_key = ""
 Bearer_token = ""
@@ -21,7 +22,8 @@ with open("tweet_api.json", "r") as temp_json:
     Access_token = temp_dict['Access_token']
     Access_secret_token = temp_dict['Access_secret_token']
     _url = temp_dict['url']
-
+    _url2 = temp_dict['url2']
+    
 def random_text():
     url = _url
     headers = {
@@ -42,6 +44,31 @@ def random_text():
     else:
         rand_index = random.randint(1,len(text_list))
         return text_list[rand_index]
+
+def random_text_ver2():
+    url = _url2
+    headers = {
+        "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    text_list = []
+    table = soup.select("tbody.hide_notice")[0]
+    for tr in table.select('tr'):
+        if tr.get('class') == None:
+            for a in tr.find_all('a'):
+                if a.get('class') == None:
+                    text_list.append(a.text)
+                    
+    if len(text_list) == 0:
+        return None
+    else:
+        rand_index = random.randint(1,len(text_list))
+        text = text_list[rand_index]
+        if text[-1].isdigit():
+            text = text[:-1]
+        return text
 
 if __name__ == '__main__': 
     #authenticating to access the twitter API
